f96f39a33ad81c3cec33e8479d2d5ba34d6f3d59
Nick Minaev <nickminaev@MacBook-Pro-sl-Nick.local>
2025-03-05 09:13:35 +0200

    fix: terraform format fix

--- infra/main.tf
+++ infra/main.tf
@@ -10,11 +10,11 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
 }
 
 data "aws_iam_policy" "lambda_deployer_permission_boundary" {
-    name = "DeployerPermissions"
+  name = "DeployerPermissions"
 }
 
 resource "aws_iam_role" "shopping_manager_lambda_role" {
-  name               = "testRole"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+  name                 = "testRole"
+  assume_role_policy   = data.aws_iam_policy_document.lambda_assume_role_policy.json
   permissions_boundary = data.aws_iam_policy.lambda_deployer_permission_boundary.arn
 }
